1bbd1467d35ae09a3491a33347ccde7430a37c67
Miguel Batilando <miguel.batilando@gmail.com>
2018-08-06 16:07:40 -0700

    added icon to launch screen

---
+++ LaunchScreen.swift
@@ -0,0 +1,16 @@
+//
+//  LaunchScreen.swift
+//  WeAchieve
+//
+//  Created by Miguel Batilando on 8/6/18.
+//  Copyright © 2018 Make School. All rights reserved.
+//
+
+import UIKit
+
+class LaunchScreen: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
